549bb1e6459ec31587f4232456705964efc2a2fb
Chris-edappady <chris.edappady123@gmail.com>
2026-01-15 12:38:00 +0400

    Update 3: Added request logger and CORS middleware

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 
+//Initializing the Express app 
 const app = express();
 app.use(express.json());
+
+
+//Request logger : logs date and time 
+app.use((req, res, next) => {
+  console.log(`[${new Date().toLocaleDateString()}] ${req.method} ${req.url}`);
+  next();
+})
+
+//CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Trash Monitoring API running');
